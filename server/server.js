1cfc5ff0116d49344b7b734db2bf0e5e481f42aa
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-01-27 08:37:54 +0000

    res.json to res.send

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.get("/", (req, res) => {
-  res.json("Service is active!")
+  res.send("Service is active!")
 })
 
 const { Server } = require("socket.io");
