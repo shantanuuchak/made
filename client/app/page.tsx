f3db2c038d07c3b36caa9a1c8167bbbad27baacf
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-01-27 09:46:06 +0000

    Create SignUp page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import { io } from "socket.io-client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HeroUIProvider } from "@heroui/react";
 import { Chat, Inputs, SignUp } from "@/components";
 
 const server = io(
-  "https://metropolitan-cherin-chak-7831167c.koyeb.app/"
+  "https://madeapp.koyeb.app/"
 );
 
 export default function Home() {
+  const [user, setUser] = useState("");
   const [animal, setAnimal] = useState("Click on button first");
 
   useEffect(() => {
@@ -23,10 +24,14 @@ export default function Home() {
 
   return (
     <HeroUIProvider>
-      <div className="min-h-screen max-h-screen max-w-screen mx-auto md:container md:p-20 md:pt-4 p-1 relative">
-        <Chat />
-        <Inputs />
-      </div>
+      {user ?
+        <div className="min-h-screen max-h-screen max-w-screen mx-auto md:container md:p-20 md:pt-4 p-1 relative">
+          <Chat />
+          <Inputs />
+        </div>
+        :
+        <SignUp onSubmit={setUser} />
+      }
     </HeroUIProvider>
   );
 }
--- client/components/SignUp.tsx
+++ client/components/SignUp.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@heroui/react";
+import { Form, Input, Button } from "@heroui/react";
+import { FormEvent } from "react";
 
-function SignUp() {
-  return <div>SignUp</div>;
+function SignUp({ onSubmit }) {
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    console.log(data.name)
+
+
+    onSubmit(data.name)
+  }
+
+  return <div className="min-h-screen max-h-screen w-full flex items-center justify-center">
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="madeapp logo"
+          height={40}
+          radius="sm"
+          src="./favicon.ico"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">MADE Room</p>
+          <p className="text-small text-default-500">made.phleebs.com</p>
+        </div>
+      </CardHeader>
+
+      <Divider />
+
+      <CardBody>
+        <Form className="w-full max-w-xs" validationBehavior="native" onSubmit={handleSubmit}>
+          <Input
+            isRequired
+            errorMessage="Please enter your name"
+            label="Name"
+            labelPlacement="outside"
+            name="name"
+            autoComplete="off"
+            placeholder="Enter your name"
+            type="text"
+          />
+          <Button type="submit" variant="bordered">
+            Submit
+          </Button>
+        </Form>
+      </CardBody>
+
+      <Divider />
+
+      <CardFooter>
+        <Link isExternal showAnchorIcon href="https://github.com/phleebs/made">
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+  </div>;
 }
 
 export default SignUp;
