39efd5058e2778de80ff273e93c56043f484766a
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-01-27 08:29:00 +0000

    Port 8000 for koyeb

--- server/server.js
+++ server/server.js
@@ -27,6 +27,6 @@ io.on("connection", (socket) => {
   })
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+server.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });
